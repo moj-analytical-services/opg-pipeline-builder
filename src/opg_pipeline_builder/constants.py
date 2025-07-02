f12cff458c512bf20e597280ec4c225e7f760001
p-sin <philip.sinfield@justice.gov.uk>
2025-07-02 12:38:25 +0100

    Move metadata model into pipeline-builder

---
+++ src/opg_pipeline_builder/constants.py
@@ -0,0 +1,23 @@
+ALLOWED_DATA_TYPES: tuple[str, ...] = (
+    "string",
+    "list<string>",
+    "int32",
+    "int64",
+    "float64",
+    "bool",
+    "date32",
+    "timestamp(ns)",
+    "timestamp(ms)",
+)
+
+ALLOWED_ETL_STAGES: tuple[str, ...] = (
+    "land",
+    "raw",
+    "raw_hist",
+    "processed",
+    "curated",
+    "derived",
+)
+
+
+ALLOWED_FILE_FORMATS: tuple[str, ...] = ("parquet", "csv", "json", "xlsx")

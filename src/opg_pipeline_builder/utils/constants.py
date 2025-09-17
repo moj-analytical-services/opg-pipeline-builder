7d60770e7685acefe83c01a357970328d679e919
p-sin <philip.sinfield@justice.gov.uk>
2025-09-17 13:30:30 +0100

    Correct unit tests

--- src/opg_pipeline_builder/utils/constants.py
+++ src/opg_pipeline_builder/utils/constants.py
@@ -1,7 +1,6 @@
 import base64
 import json
 import os
-import re
 from ast import literal_eval
 from binascii import Error
 from datetime import datetime
@@ -375,52 +374,51 @@ def get_dag_timestamp() -> Union[int, None]:
     Union[int, None]
         Timestamp int for DAG run or None, if not applicable
     """
-    mp_args = get_multiprocessing_settings()
+    # mp_args = get_multiprocessing_settings()
 
-    raise_error = False
-
+    # raise_error = False
     try:
         dag_ts = int(os.environ["RUN_TIMESTAMP"])
         return dag_ts
     except Exception:  # pylint: disable=broad-exception-caught
         return None
 
-    if mp_args is not None:
-        tmp_staging = mp_args.get("temp_staging", False)
-        if tmp_staging is True:
-            raise_error = True
+    # if mp_args is not None:
+    #     tmp_staging = mp_args.get("temp_staging", False)
+    #     if tmp_staging is True:
+    #         raise_error = True
 
-    try:
-        dag_run_id = os.environ["DAG_RUN_ID"]
-        dag_interval_end = os.environ["DAG_INTERVAL_END"]
+    # try:
+    #     dag_run_id = os.environ["DAG_RUN_ID"]
+    #     dag_interval_end = os.environ["DAG_INTERVAL_END"]
 
-        manual_run = re.search("manual__", dag_run_id)
+    #     manual_run = re.search("manual__", dag_run_id)
 
-        try:
-            if manual_run is not None:
-                dag_ts = int(
-                    datetime.fromisoformat(
-                        dag_run_id.replace("manual__", "")
-                    ).timestamp()
-                )
+    #     try:
+    #         if manual_run is not None:
+    #             dag_ts = int(
+    #                 datetime.fromisoformat(
+    #                     dag_run_id.replace("manual__", "")
+    #                 ).timestamp()
+    #             )
 
-            else:
-                dag_ts = int(datetime.fromisoformat(dag_interval_end).timestamp())
+    #         else:
+    #             dag_ts = int(datetime.fromisoformat(dag_interval_end).timestamp())
 
-        except Exception as e:
-            raise ValueError(
-                f"""
-                DAG_RUN_TIMESTAMP must be a string in ISO format.
-                Error: {e}
-                """
-            )
+    #     except Exception as e:
+    #         raise ValueError(
+    #             f"""
+    #             DAG_RUN_TIMESTAMP must be a string in ISO format.
+    #             Error: {e}
+    #             """
+    #         )
 
-    except KeyError:
-        if raise_error:
-            raise ValueError(
-                "DAG_RUN_ID and DAG_INTERVAL_END must be set when temp staging enabled."
-            )
-        else:
-            dag_ts = None
+    # except KeyError:
+    #     if raise_error:
+    #         raise ValueError(
+    #             "DAG_RUN_ID and DAG_INTERVAL_END must be set when temp staging enabled."
+    #         )
+    #     else:
+    #         dag_ts = None
 
-    return dag_ts
+    # return dag_ts
